195ed6791559ee3453401296c6fc56a95357e0e5
arjawa <arjawa123@gmail.com>
2021-03-12 23:21:37 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+		screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+		fontFamily: {
+      kumbh: ['Kumbh Sans', 'sans-serif']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
